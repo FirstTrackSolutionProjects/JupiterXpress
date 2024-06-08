992492f7ff29072b59cc91cec6169fda57bb796f
AdityaKumar5155 <adityakumar5155@gmail.com>
2024-06-08 18:56:31 +0530

    img

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -7,10 +7,13 @@ import ReasonCard from "../Components/ReasonCard"
 const About = () => {
   return (
     <>
-    
-    <div className=' space-y-10 min-h-screen px-6 md:px-12 py-32 bg-center  bg-no-repeat bg-fixed bg-[url("contactbg.jpg")] bg-cover flex flex-col justify-center items-center overflow-auto'>
     <Header />
-     <div className="w-full space-y-5 max-w-[1024px] border p-4 rounded-xl bg-[rgba(255,255,255,0.7)]">
+    
+    <div className='relative space-y-10 min-h-screen px-6 md:px-12 py-32 z-[-1] bg-center  bg-no-repeat bg-fixed bg-[url("contactbg.jpg")] bg-cover flex flex-col justify-center items-center overflow-auto'>
+    <div className="absolute z-0 h-full w-full bg-[rgba(0,0,255,0.1)]">
+
+    </div>
+     <div className="w-full z-10 space-y-5 max-w-[1024px] border p-4 rounded-xl bg-[rgba(255,255,255,0.7)]">
             <div className="font-medium text-2xl text-center">Who we are?</div>
             <div>
                 Jupiter Xpress Delivery is a leading provider of efficient and reliable delivery services, committed to simplifying logistics for businesses and individuals. Our mission is to ensure swift and secure delivery of a wide range of products, including electronics, clothing, documents, perishables, and more. 
@@ -25,7 +28,7 @@ const About = () => {
                 Experience convenience and efficiency with Jupiter Xpress Delivery - your reliable partner in logistics solutions.
             </div>
      </div>
-     <div className="w-full space-y-10 max-w-[1024px] border px-4 py-10 rounded-xl bg-[rgba(255,255,255,0.7)]">
+     <div className="w-full z-10 space-y-10 max-w-[1024px] border px-4 py-10 rounded-xl bg-[rgba(255,255,255,0.7)]">
             <div className="font-medium text-2xl text-center">Our Services</div>
             <div className="w-full flex justify-evenly">
                 <ServiceCard color={'blue-600'}/>
@@ -34,7 +37,7 @@ const About = () => {
                 <ServiceCard color={'blue-600'}/>
             </div>
      </div>
-     <div className="w-full space-y-10 max-w-[1024px] border px-4 py-10 rounded-xl bg-[rgba(255,255,255,0.7)]">
+     <div className="w-full z-10 space-y-10 max-w-[1024px] border px-4 py-10 rounded-xl bg-[rgba(255,255,255,0.7)]">
             <div className="font-medium text-2xl text-center">Why Jupiter Xpress</div>
             <div className="space-y-3">
             <div className="w-full flex justify-evenly">
