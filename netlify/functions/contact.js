7f04d701c2d7bc3af306fe5b687e56178e9ef703
AdityaKumar5155 <adityakumar5155@gmail.com>
2024-06-10 02:55:02 +0530

    contact

---
+++ netlify/functions/contact.js
@@ -0,0 +1,55 @@
+// netlify/functions/authenticate.js
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+require('dotenv').config();
+
+
+exports.handler = async (event, context) => {
+  if (event.httpMethod !== 'POST') {
+    return {
+      statusCode: 405,
+      body: JSON.stringify({ message: 'Method Not Allowed' }),
+    };
+  }
+
+  const { name, email,  mobile, subject ,message } = JSON.parse(event.body);
+
+
+  try {
+    let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.EMAIL_USER, // replace with your email
+            pass: process.env.EMAIL_PASS  // replace with your email password or app password
+        }
+    });
+
+    // Setup email data
+    let mailOptions = {
+        from: process.env.EMAIL_USER,
+        // to: 'info@firsttracksolution.tech', // replace with the recipient's email
+        to: process.env.CONTACT_SERVICE_EMAIL, // replace with the recipient's email
+        subject: subject,
+        text: "Name: " + name + '\n' + "E-mail: " + email + '\n' + "Mobile: " + mobile + '\n'+ "Message: " + message
+    };
+
+    // Send mail with defined transport object
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return {
+                statusCode: 500,
+                body: JSON.stringify({ message: error.toString() , success: false }),
+              };
+        }
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ message: "success" , success: true }),
+          };
+    });
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: error.message , success: false }),
+    };
+  }
+};
--- src/Components/ContactForm.jsx
+++ src/Components/ContactForm.jsx
@@ -1,8 +1,47 @@
 
 const ContactForm = () => {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const email = (document.querySelector('input[name="email"]')).value;
+    const name = (document.querySelector('input[name="name"]')).value;
+    const message = (document.querySelector('textarea[name="message"]')).value;
+    const subject = (document.querySelector('input[name="subject"]')).value;
+    const mobile = (document.querySelector('input[name="mobile"]')).value;
+
+    const data = {
+      name : name,
+      email : email,
+      message : message,
+      mobile : mobile,
+      subject : subject,
+    };
+
+    // Make the API call
+    fetch('/.netlify/functions/contact', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+      .then(response => response.json())
+      .then(result => {
+        if (result.success) {
+          alert("Email sent successfully");
+          // Handle successful login
+        } else {
+          alert('Email failed: ' + result.message);
+          // Handle login failure
+        }
+      })
+      .catch(error => {
+        console.error('Error:', error);
+        alert('An error occurred during Email'+data.email +data.message +data.name);
+      });
+    }
   return (
     <div className="flex flex-col w-full lg:w-auto space-y-6 items-center text-black my-12 font-medium">
-      <form action="" className="flex flex-col space-y-2 w-full sm:w-[500px]">
+      <form action="" onSubmit={handleSubmit} className="flex flex-col space-y-2 w-full sm:w-[500px]">
         <input type="text" name="name" placeholder="Full Name" className="py-2 px-4 rounded-3xl" />
         <input type="email" name="email" placeholder="E-mail Address" className="py-2 px-4 rounded-3xl"/>
         <input type="text" name="mobile" placeholder="Mobile Number" className="py-2 px-4 rounded-3xl"/>
