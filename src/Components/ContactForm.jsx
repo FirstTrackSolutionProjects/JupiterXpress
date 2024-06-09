a118a69982211c0dcc32376343f6843eec3da90d
AdityaKumar5155 <adityakumar5155@gmail.com>
2024-06-10 03:06:42 +0530

    contact

--- src/Components/ContactForm.jsx
+++ src/Components/ContactForm.jsx
@@ -1,19 +1,28 @@
-
+import { useState } from "react";
 const ContactForm = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    mobile: '',
+    message: '',
+    subject: ''
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value
+    }));
+  };
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const email = (document.querySelector('input[name="email"]')).value;
-    const name = (document.querySelector('input[name="name"]')).value;
-    const message = (document.querySelector('textarea[name="message"]')).value;
-    const subject = (document.querySelector('input[name="subject"]')).value;
-    const mobile = (document.querySelector('input[name="mobile"]')).value;
-
     const data = {
-      name : name,
-      email : email,
-      message : message,
-      mobile : mobile,
-      subject : subject,
+      name : formData.name,
+      email : formData.email,
+      message : formData.message,
+      mobile :formData.mobile,
+      subject :formData.subject,
     };
 
     // Make the API call
@@ -42,11 +51,11 @@ const ContactForm = () => {
   return (
     <div className="flex flex-col w-full lg:w-auto space-y-6 items-center text-black my-12 font-medium">
       <form action="" onSubmit={handleSubmit} className="flex flex-col space-y-2 w-full sm:w-[500px]">
-        <input type="text" name="name" placeholder="Full Name" className="py-2 px-4 rounded-3xl" />
-        <input type="email" name="email" placeholder="E-mail Address" className="py-2 px-4 rounded-3xl"/>
-        <input type="text" name="mobile" placeholder="Mobile Number" className="py-2 px-4 rounded-3xl"/>
-        <input type="subject" name="subject" placeholder="Subject" className="py-2 px-4 rounded-3xl"/>
-        <textarea name="message" placeholder="Message" className="py-2 px-4 h-32 rounded-3xl"/>
+        <input type="text" value={formData.name}  onChange={handleChange} name="name" placeholder="Full Name" className="py-2 px-4 rounded-3xl" />
+        <input type="email" value={formData.email} onChange={handleChange} name="email" placeholder="E-mail Address" className="py-2 px-4 rounded-3xl"/>
+        <input type="text" value={formData.mobile} onChange={handleChange} name="mobile" placeholder="Mobile Number" className="py-2 px-4 rounded-3xl"/>
+        <input type="subject" value={formData.subject} onChange={handleChange} name="subject" placeholder="Subject" className="py-2 px-4 rounded-3xl"/>
+        <textarea name="message" value={formData.message} onChange={handleChange} placeholder="Message" className="py-2 px-4 h-32 rounded-3xl"/>
         <button type="submit" className="px-4 py-2 border border-white text-white rounded-3xl">Send</button>
       </form>
     </div>
