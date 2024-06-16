d36211e406ebb6f39073069fb751056daee00ddf
AdityaKumar5155 <adityakumar5155@gmail.com>
2024-06-17 03:42:46 +0530

    hi

--- src/Components/CreateOrder.jsx
+++ src/Components/CreateOrder.jsx
@@ -7,13 +7,14 @@ const ComparePrices = ({method, status, origin, dest, weight, payMode, codAmount
   const [price,setPrice] = useState(null)
   useEffect(()=>{
     const data = async () => {
-      await fetch(`/.netlify/functions/price?method=${method}&status=${status}&origin=${origin}&dest=${dest}&weight=${weight}&payMode=${payMode}&codAmount=${codAmount}`, {
-        method: 'GET',
+      await fetch(`/.netlify/functions/price`, {
+        method: 'POST',
         headers: { 'Accept': '*/*',
           'Content-Type': 'application/json',
           'Authorization': 'Token 2e80e1f3f5368a861041f01bb17c694967e94138',
           "Access-Control-Allow-Origin" : "*",
-          "Access-Control-Allow-Headers" : "Origin, X-Requested-With, Content-Type, Accept"
+          "Access-Control-Allow-Headers" : "Origin, X-Requested-With, Content-Type, Accept",
+          body : JSON.stringify({method, status, origin, dest, weight, payMode, codAmount})
         }
       }).then(response => response.json()).then(result => {console.log(result.price); setPrice(result.price)}).catch(error => console.log(error))
     }  
