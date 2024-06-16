a7203a04d579980e4158f042299b252c6adb1ed9
AdityaKumar5155 <adityakumar5155@gmail.com>
2024-06-17 03:13:18 +0530

    hi

--- netlify/functions/price.js
+++ netlify/functions/price.js
@@ -7,10 +7,9 @@ exports.handler = async (event, context) => {
   try {
     const response = await fetch(`https://track.delhivery.com/api/kinko/v1/invoice/charges/.json?md=${method}&ss=${status}&d_pin=${dest}&o_pin=${origin}&cgm=${weight}&pt=${payMode}&cod=${codAmount}`);
     const data = await response.json();
-    const price = data[0]['total_amount']
     return {
       statusCode: 200,
-      body: JSON.stringify({price}),
+      body: JSON.stringify({data}),
       headers: {
         'Content-Type': 'application/json',
         'Access-Control-Allow-Origin': '*', // Allow all origins (CORS)
