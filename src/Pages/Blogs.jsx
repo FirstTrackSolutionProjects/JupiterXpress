b775eea00dd1729ae4143177bcdb0277bccedc85
AdityaKumar5155 <adityakumar5155@gmail.com>
2024-06-24 15:12:52 +0530

    blog

--- src/Pages/Blogs.jsx
+++ src/Pages/Blogs.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 import Header from '../Components/Header'
 
 
-const Blog = () => {
+const Blog = ({img, title}) => {
   return (
     <>
-      <div className=' bg-white w-72 rounded-xl flex flex-col '>
+      <div className=' bg-white w-72 rounded-xl flex flex-col my-3 mx-3'>
       <div className='w-72  bg-white rounded-l-xl overflow-hidden flex items-center'>
-          <img src="tech.jpg" className='object-contain' alt="" />
+          <img src={img} className='object-contain' alt="" />
         </div> 
         <div className=' p-4'>
-          <p className='text-xl font-medium'>Launching new features in 2024</p>
+          <p className='text-xl font-medium'>{title}</p>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor eos obcaecati neque autem ullam, pariatur non inventore aperiam porro quasi officia officiis exercitationem nemo totam.</p>
         </div>
       </div>
@@ -26,10 +26,10 @@ const Blogs = () => {
     <div className="w-full relative z-20 space-y-5 max-w-[1024px] border p-4 mt-12 sm:mt-16 rounded-xl bg-[rgba(67,158,245,0.3)]">
       <p className=' text-white text-2xl font-medium text-center'>YOUR SATISFACTION, OUR STORY</p>
       </div>
-      <div className='w-full relative z-20  max-w-[1024px] p-4 mt-12 sm:mt-16 rounded-xl flex justify-between'>
-        <Blog />
-        <Blog />
-        <Blog />
+      <div className='w-full relative z-20  max-w-[1024px] p-4 mt-12 sm:mt-16 rounded-xl flex flex-wrap justify-evenly'>
+        <Blog img={'https://jupiterxpress.online/public/uploads/blogs/20240523113104.jpeg'} title={'Revolutionizing Courier Services: Meet Jupiter Xpress'} desc={''} />
+        <Blog img={'https://jupiterxpress.online/public/uploads/blogs/20240523113304.png'} title={'Beyond Delivery: The Jupiter Xpress Advantage'} desc={''} />
+        <Blog img={'https://jupiterxpress.online/public/uploads/blogs/20240523113637.jpg'} title={'Delivering Excellence: Why Jupiter Xpress is Your Ultimate Courier Solution'} desc={''} />
       </div>
     </div>
     </>
