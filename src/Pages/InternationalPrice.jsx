cf5a328035574c6deee5496c16dfc7f67a5f1148
AdityaKumar5155 <adityakumar5155@gmail.com>
2024-09-30 11:44:44 +0530

    chore : removed Header and Footer from InternationalPrice.jsx

--- src/Pages/InternationalPrice.jsx
+++ src/Pages/InternationalPrice.jsx
@@ -1,14 +1,10 @@
 import React from 'react'
-import Header from '../Components/Header'
-import Footer from '../Components/Footer'
 import InternationalPriceInquiry from '../Components/InternationalPriceInquiry'
 
 const Price = () => {
   return (
     <>
-        <Header />
         <InternationalPriceInquiry />
-        <Footer />
     </>
   )
 }
