b13a4c8a7dd54a58362b7c2e2f57ef285b65fd28
AdityaKumar5155 <adityakumar5155@gmail.com>
2024-09-30 11:43:29 +0530

    chore : removed Header and Footer from DomesticPrice.jsx

--- src/Pages/DomesticPrice.jsx
+++ src/Pages/DomesticPrice.jsx
@@ -1,14 +1,10 @@
 import React from 'react'
-import Header from '../Components/Header'
-import Footer from '../Components/Footer'
 import PriceCalc from '../Components/PriceCalc'
 
 const Price = () => {
   return (
     <>
-        <Header />
         <PriceCalc />
-        <Footer />
     </>
   )
 }
