7e610f8ce54edc607a57224f93bb1a67223e9740
AdityaKumar5155 <adityakumar5155@gmail.com>
2024-09-30 11:41:29 +0530

    chore : removed Header from Blogs.jsx

--- src/Pages/Blogs.jsx
+++ src/Pages/Blogs.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Header from '../Components/Header'
 
 const Blog = ({img, title, desc}) => {
   return (
@@ -20,7 +19,6 @@ const Blog = ({img, title, desc}) => {
 const Blogs = () => {
   return (
     <>
-    <Header/>
     <div className={`relative space-y-10 min-h-screen p-6 md:p-12 z-0 bg-center  bg-no-repeat bg-fixed bg-[url('/src/assets/blog.jpg')] bg-cover flex flex-col items-center overflow-auto`}>
     <div className="w-full relative z-20 space-y-5 max-w-[1024px] border p-4 mt-12 sm:mt-16 rounded-xl bg-[rgba(67,158,245,0.3)]">
       <p className=' text-white text-2xl font-medium text-center'>YOUR SATISFACTION, OUR STORY</p>
