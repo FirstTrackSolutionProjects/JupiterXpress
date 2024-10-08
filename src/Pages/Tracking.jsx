049257f7222d7863fc5b7b9449bf1fbdc3f4db66
AdityaKumar5155 <adityakumar5155@gmail.com>
2024-10-09 00:47:11 +0530

    feat : added pickrr tracking

--- src/Pages/Tracking.jsx
+++ src/Pages/Tracking.jsx
@@ -1,14 +1,14 @@
 import React, { useEffect, useState } from 'react'
 const API_URL = import.meta.env.VITE_APP_API_URL
 const Form = () => {
-    const [formData,setFormData] = useState({
-        awb : ''
+    const [formData, setFormData] = useState({
+        awb: ''
     })
 
     useEffect(() => {
-        if (localStorage.getItem('track')){
-            setFormData({id: localStorage.getItem('track'), isWaybill: true})
-            localStorage.setItem('track','')
+        if (localStorage.getItem('track')) {
+            setFormData({ id: localStorage.getItem('track'), isWaybill: true })
+            localStorage.setItem('track', '')
             // handleSubmit(1)
         }
     }, [])
@@ -16,15 +16,15 @@ const Form = () => {
     const handleChange = (e) => {
         const { name, value, type, checked } = e.target;
         setFormData((prevData) => ({
-          ...prevData,
-          [name]:type === 'radio' ? checked : value
+            ...prevData,
+            [name]: type === 'radio' ? checked : value
         }));
-      };
-    const [trackingData,setTrackingData] = useState(null)
+    };
+    const [trackingData, setTrackingData] = useState(null)
     const handleSubmit = async (e) => {
-        try{
+        try {
             e.preventDefault();
-        } catch (e) {}
+        } catch (e) { }
         const data = await fetch(`${API_URL}/shipment/track`, {
             method: 'POST',
             headers: {
@@ -40,9 +40,9 @@ const Form = () => {
             <div className='w-full pt-16'>
                 <div className='w-full p-8 flex flex-col items-center space-y-16 mb-8'>
                     <div className='text-center text-3xl font-medium'>Track your Parcel</div>
-                    
-        <form className="flex flex-col items-center  space-y-8" onSubmit={handleSubmit}>
-            {/* <div className='flex space-x-8'>
+
+                    <form className="flex flex-col items-center  space-y-8" onSubmit={handleSubmit}>
+                        {/* <div className='flex space-x-8'>
 
             <div className='flex space-x-1'>
             <input type="radio" id='awb' name='isWaybill' checked value={true} onChange={handleChange} />
@@ -53,11 +53,11 @@ const Form = () => {
             <label htmlFor="order">Order Id</label>
             </div>
             </div> */}
-            <div className='flex'>
-            <input type="text" name="awb" value={formData.id} onChange={handleChange} className="border py-2 px-4 sm:rounded-l-xl bg-blue-50" placeholder="Enter Tracking Id/AWB" />
-            <button className="border py-2 px-4 sm:rounded-r-xl bg-blue-50">Track</button>
-            </div>
-        </form>
+                        <div className='flex'>
+                            <input type="text" name="awb" value={formData.id} onChange={handleChange} className="border py-2 px-4 sm:rounded-l-xl bg-blue-50" placeholder="Enter Tracking Id/AWB" />
+                            <button className="border py-2 px-4 sm:rounded-r-xl bg-blue-50">Track</button>
+                        </div>
+                    </form>
                 </div>
             </div>
             {trackingData && <Result data={trackingData} />}
@@ -65,7 +65,7 @@ const Form = () => {
     )
 }
 
-const Card = ({scan}) => {
+const Card = ({ scan }) => {
     return (
         <>
             <div className="w-full h-16 bg-white relative items-center px-8 flex border-b space-x-4">
@@ -76,10 +76,10 @@ const Card = ({scan}) => {
         </>
     )
 }
-const FlightGoCard = ({scan}) => {
+const FlightGoCard = ({ scan }) => {
     return (
         <>
-         <div className="w-full h-16 bg-white relative items-center px-8 flex border-b space-x-4">
+            <div className="w-full h-16 bg-white relative items-center px-8 flex border-b space-x-4">
                 <div>{scan.event_at}</div>
                 <div>{scan.event_location}</div>
                 <div className="absolute right-8 cursor-pointer">{scan.event_description}</div>
@@ -87,10 +87,10 @@ const FlightGoCard = ({scan}) => {
         </>
     )
 }
-const MovinCard = ({scan}) => {
+const MovinCard = ({ scan }) => {
     return (
         <>
-         <div className="w-full h-16 bg-white relative items-center px-8 flex border-b space-x-4">
+            <div className="w-full h-16 bg-white relative items-center px-8 flex border-b space-x-4">
                 <div>{scan.timestamp}</div>
                 <div className="absolute right-8 cursor-pointer">{scan.package_status}</div>
             </div>
@@ -98,34 +98,49 @@ const MovinCard = ({scan}) => {
     )
 }
 
-const Result = ({data}) => {
+const ShipRocketCard = ({ scan }) => {
+    return (
+        <>
+            <div className="w-full h-16 bg-white relative items-center px-8 flex border-b space-x-4">
+                <div>{scan.timestamp}</div>
+                <div>{scan.location}</div>
+                <div className="absolute right-8 cursor-pointer">{scan.remarks}</div>
+            </div>
+        </>
+    )
+}
+
+const Result = ({ data }) => {
     useEffect(() => {
         console.log(data)
-    },[data])
+    }, [data])
     return (
         <>
             <div className={`w-full p-8 overflow-hidden  `}>
-                    {data?.id == 1 ? data?.data.ShipmentData[0].Shipment.Scans.slice().reverse().map((scan, index)=>(
-                        <Card key={index} scan={scan.ScanDetail} />
-                    )) : null}
-                    {data?.id == 2 ? data?.data.docket_events.map((scan, index)=>(
-                        <FlightGoCard key={index} scan={scan} />
-                    )): null}
-                    {data?.id == 3 ? data?.data.map((scan, index)=>(
-                        <MovinCard key={index} scan={scan} />
-                    )): null}
+                {data?.id == 1 ? data?.data.ShipmentData[0].Shipment.Scans.slice().reverse().map((scan, index) => (
+                    <Card key={index} scan={scan.ScanDetail} />
+                )) : null}
+                {data?.id == 2 ? data?.data.docket_events.map((scan, index) => (
+                    <FlightGoCard key={index} scan={scan} />
+                )) : null}
+                {data?.id == 3 ? data?.data.map((scan, index) => (
+                    <MovinCard key={index} scan={scan} />
+                )) : null}
+                {data?.id == 4 ? data?.data.map((scan, index) => (
+                    <ShipRocketCard key={index} scan={scan} />
+                )) : null}
             </div>
-        
+
         </>
     )
 }
 
 const DomesticTracking = () => {
-  return (
-    <>
-        <Form />
-    </>
-  )
+    return (
+        <>
+            <Form />
+        </>
+    )
 }
 
 export default DomesticTracking
