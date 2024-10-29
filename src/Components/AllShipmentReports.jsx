7e7763b9880c15ad6035fff2aa5bd61b6d55c005
AdityaKumar5155 <adityakumar5155@gmail.com>
2024-10-29 13:52:07 +0530

    chore : tweaked the styling of filter

--- src/Components/AllShipmentReports.jsx
+++ src/Components/AllShipmentReports.jsx
@@ -328,7 +328,7 @@ const Listing = () => {
           <summary>Filters</summary>
           <div className="grid space-y-2 lg:grid-rows-1 lg:grid-cols-4 lg:space-y-0 lg:space-x-4 p-2 rounded-xl w-full bg-blue-500 text-black justify-evenly">
             <input
-              className="p-1 rounded-xl"
+              className="p-1 rounded-xl min-w-[260px] lg:min-w-0"
               type="text"
               name="name"
               placeholder="Merchant Name"
