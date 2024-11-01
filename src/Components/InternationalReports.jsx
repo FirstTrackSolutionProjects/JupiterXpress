7a0dba22b5ac42a6865cb9072cd85f57e4239787
AdityaKumar5155 <adityakumar5155@gmail.com>
2024-11-02 02:58:29 +0530

    feat : implemented international shipment creation date

--- src/Components/InternationalReports.jsx
+++ src/Components/InternationalReports.jsx
@@ -54,13 +54,14 @@ const Card = ({ report }) => {
       {view && <View report={report} setIsView={setIsView}/>}
       <div className="w-full h-24 bg-white relative items-center px-4 sm:px-8 flex border-b">
       <div className="text-[10px] text-gray-500">
-          <div className="text-sm font-bold">{report.ref_id}</div>
+          <div className="text-sm font-bold text-black">{report.ref_id} <span className="text-gray-500">{`(${report.iid})`}</span></div>
           {
             report.isAdmin? <><div >{report.fullName}</div>
             <div >{report.email}</div></> : <><div >{report.consignee_name}</div>
             </>
           }
             <div> {report.awb?`AWB : ${report.awb}`:null}</div>
+            <div>{report.date ? report.date.toString().split('T')[0] + ' ' + report.date.toString().split('T')[1].split('.')[0] : null}</div>
           </div>
         <div className="absolute right-4 sm:right-8 flex space-x-2">
         {/* {report.status} */}
@@ -85,7 +86,7 @@ const Listing = () => {
           .then(response => response.json())
           .then(result => {
             if (result.success) {
-              result.order.sort((a,b) => parseInt(a.iid) - parseInt(b.iid)).reverse();
+              result.order.sort((a,b) => a.date - b.date).reverse();
               setReports(result.order);
             } else {
               alert('Fetch failed: ' + result.message)
