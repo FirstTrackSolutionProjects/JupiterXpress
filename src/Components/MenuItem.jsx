06c310540463e19b423fc71a41d73be2667ec69f
AdityaKumar5155 <adityakumar5155@gmail.com>
2024-11-03 04:47:07 +0530

    feat : active menu item will have different bg color

--- src/Components/MenuItem.jsx
+++ src/Components/MenuItem.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useLocation, useNavigate } from 'react-router-dom'
 
 const MenuItem = ({icon, name, url, isDropdown, dropDownOptions, setShowRecharge}) => {
@@ -6,9 +6,12 @@ const MenuItem = ({icon, name, url, isDropdown, dropDownOptions, setShowRecharge
   const navigate = useNavigate()
   const [isOpen, setIsOpen] = useState(0)
   const [isCurrentMenu, setIsCurrentMenu] = useState(location.pathname === `/dashboard/${url}`)
+  useEffect(() => {
+    setIsCurrentMenu(location.pathname === `/dashboard/${url}`)
+  },[navigate])
   return (
     <>
-    <div onClick={isDropdown?()=>setIsOpen(!isOpen):(name==="Wallet Recharge"?()=>setShowRecharge(true):()=>navigate(url))} className=' cursor-pointer px-2 w-full h-12 bg-white transition-all duration-300 hover:bg-[rgba(37,168,229,0.6)] relative flex items-center border-r-2 border-b-2'>
+    <div onClick={isDropdown?()=>setIsOpen(!isOpen):(name==="Wallet Recharge"?()=>setShowRecharge(true):()=>navigate(url))} className={` cursor-pointer px-2 w-full h-12 ${isCurrentMenu?'bg-[rgba(37,168,229,0.6)]':'bg-white'} transition-all duration-300 hover:bg-[rgba(37,168,229,0.6)] relative flex items-center border-r-2 border-b-2`}>
       <img src={icon} alt="" className='w-12' />
       <p className=''>{name}</p>
       {isDropdown ? <p className={`absolute transition-transform duration-300 ${isOpen?"rotate-90":""} right-1`}>
