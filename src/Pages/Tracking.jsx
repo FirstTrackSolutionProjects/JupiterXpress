a19d86742b772edf26525e7830ba10db0febc9a3
AdityaKumar5155 <adityakumar5155@gmail.com>
2024-11-06 03:10:38 +0530

    feat : added shiprocket tracking card

--- src/Pages/Tracking.jsx
+++ src/Pages/Tracking.jsx
@@ -98,7 +98,7 @@ const MovinCard = ({ scan }) => {
     )
 }
 
-const ShipRocketCard = ({ scan }) => {
+const PickrrCard = ({ scan }) => {
     return (
         <>
             <div className="w-full py-3 bg-white relative items-center justify-center px-8 flex border-b space-x-4">
@@ -130,6 +130,20 @@ const DillikingCard = ({ scan }) => {
     )
 }
 
+const ShiprocketCard = ({ scan }) => {
+    return (
+    <>
+        <div className="w-full py-3 bg-white relative items-center justify-center px-8 flex border-b space-x-4">
+            <div className='flex flex-col items-center justify-center'>
+                <div className='font-bold'>{scan["sr-status-label"]}</div>
+                <div>{scan.location}</div>
+                <div>{scan.date}</div>
+            </div>
+        </div>
+    </>
+    )
+}
+
 const Result = ({ data }) => {
     useEffect(() => {
         console.log(data)
@@ -147,10 +161,13 @@ const Result = ({ data }) => {
                     <MovinCard key={index} scan={scan} />
                 )) : null}
                 {data?.id == 4 ? data?.data.reverse().map((scan, index) => (
-                    <ShipRocketCard key={index} scan={scan} />
+                    <PickrrCard key={index} scan={scan} />
                 )) : null}
                 {data?.id == 5? data?.data.map((scan, index) => (
                     <DillikingCard key={index} scan={scan} />
+                )) : null}
+                {data?.id == 6? data?.data.map((scan, index) => (
+                    <ShiprocketCard key={index} scan={scan} />
                 )) : null}
             </div>
 
