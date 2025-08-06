acdc1bbbcd40f2fa7d361941b9b0dd07fee21afb
AdityaKumar5155 <adityakumar5155@gmail.com>
2025-08-06 16:58:35 +0530

    fix: conversion of UTC was incorrect

--- src/helpers/convertToUTCISOString.js
+++ src/helpers/convertToUTCISOString.js
@@ -1,7 +1,5 @@
 const convertToUTCISOString = (inputTime) => {
-  const date = new Date(inputTime);
-  const utcDate = new Date(date.getTime() + date.getTimezoneOffset() * 60000);
-  return utcDate.toISOString();
+  return new Date(inputTime).toISOString();
 };
 
 export default convertToUTCISOString;
